0b050ea2cc039867acc930873654ff7acb738d57
drishyasingla31 <drishyasingla31@gmail.com>
2026-02-27 22:31:05 +0530

    sync of change

--- Swap/Swap.c
+++ Swap/Swap.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int a=5, b=2,c;
-    c=a;
-    a=b;
-    b=c;
-    printf("%d,%d",a,b);
+int main()
+{
+    int a = 5, b = 2, c;
+    c = a;
+    a = b;
+    b = c;
+    printf("%d,%d", a, b);
     return 0;
-
 }
